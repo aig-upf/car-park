149041e77d87c5e25e052983697abee42b508a68
Andreas Kaltenbrunner <kaltenbrunner@gmail.com>
2022-03-01 18:59:30 +0100

    added testErrorplots

--- car_park_functions.py
+++ car_park_functions.py
@@ -7,7 +7,7 @@ Created on Fri Dec  3 10:49:06 2021
 """
 
 from matplotlib import pyplot as plt
-from scipy.stats import truncnorm 
+from scipy.stats import truncnorm
 import numpy as np
 
 import calendar
@@ -16,40 +16,40 @@ def getDayName(d):
     return calendar.day_name[d.weekday()]
 
 def get_days_of_protos_normalized(proto_name, df_):
-    data_temp = df_[df_['Profile_3'] == proto_name] 
+    data_temp = df_[df_['Profile_3'] == proto_name]
     days = []
     for i in range(0,data_temp.shape[0], 48):
         day = data_temp['Normalized_occupancy'][i:i+48]
         if len(day) == 48:
             days.append(day)
-        
+
     return days
 
 
 def get_days_of_protos_areanormalized(proto_name, df_):
-    data_temp = df_[df_['Profile_3'] == proto_name] 
+    data_temp = df_[df_['Profile_3'] == proto_name]
     days = []
     for i in range(0,data_temp.shape[0], 48):
         day = data_temp['Area_Normalized_occupancy'][i:i+48]
         if len(day) == 48:
             days.append(day)
-        
+
     return days
 
 
 def get_dates_of_protos(proto_name, df_):
-    data_temp = df_[df_['Profile_3'] == proto_name] 
+    data_temp = df_[df_['Profile_3'] == proto_name]
     dates = []
     for i in range(0,data_temp.shape[0], 48):
         day = data_temp['Occupancy'][i:i+48]
         t_date = data_temp.iloc[i]['Date']
         if len(day) == 48:
-            dates.append(t_date)     
+            dates.append(t_date)
     return dates
 
 
 def get_parkingfull_of_protos(proto_name, df_):
-    data_temp = df_[df_['Profile_3'] == proto_name] 
+    data_temp = df_[df_['Profile_3'] == proto_name]
     isfull = []
     for i in range(0,data_temp.shape[0], 48):
         intervallIsfull = data_temp['Free slots'][i:i+48]==0
@@ -74,13 +74,13 @@ def classify_3_proto(x):
         return 'Weekend'
     else:
         return 'Friday'
-    
+
 
 # 1 week data = 336 rows (48*7) WE ASUME WE HAVE COMPLETE DATA WITHOUT GAPS BETWEEN HOURS, AND DATA FROM 0.0 TO 23.5
 # @from_end: if the testing data is retrieved from the last part of the df or from th beginning. By default, from the end
 # @limit_date: if we want to get the train from specific date instead of by number of weeks
 
-def split_data(df, n_test_weeks, limit_date = None, from_end=True): 
+def split_data(df, n_test_weeks, limit_date = None, from_end=True):
     if(limit_date != None):
         init_test = df[df['Date']==limit_date].index[0];
         end_test = df.shape[0]
@@ -94,14 +94,14 @@ def split_data(df, n_test_weeks, limit_date = None, from_end=True):
             init_test = end_test-week_domain
             test_domain = range(init_test, end_test)
             test_domain = list(test_domain)
-            
+
         elif(from_end == False):
             week_domain = n_test_weeks*336
             init_test = 0
             end_test = week_domain
             test_domain = range(0,week_domain)
             test_domain = list(test_domain)
-            
+
     test_df = df[init_test:end_test]
     training_df = df.drop(test_domain)
     return training_df, test_df
@@ -115,7 +115,7 @@ def generate_cdf(data):
         cumulative.append(sum(data[:ii]))
     return cumulative/sum(data)
 
- 
+
 def tn(x, loc, scale):
     a = -loc/scale
     b = (1-loc)/scale
@@ -128,23 +128,23 @@ def tn_cdf(x, loc, scale):
     return truncnorm.cdf(x, a, b, loc, scale)
 
 
-def model_tn_areaN_args(params,trainining_norm,errors): 
+def model_tn_areaN_args(params,trainining_norm,errors):
     loc_ar = params[0]
     scale_ar = params[1]
     loc_de = params[2]
     scale_de = params[3]
- 
+
     num_training_days = len(trainining_norm)
     time_tn = np.linspace(0,23.5,48)/24
-    
+
     cdf_ar=tn_cdf(time_tn, loc_ar, scale_ar)
     cdf_de=tn_cdf(time_tn, loc_de, scale_de)
 
     res = cdf_ar - cdf_de
     res_n = res/sum(res)
-  
 
-    #error = 0  
+
+    #error = 0
     for ii in range(0,num_training_days):
         day = trainining_norm[ii]
         #error += np.sum(np.power(res_n - day, 2))
@@ -152,22 +152,22 @@ def model_tn_areaN_args(params,trainining_norm,errors):
     #return error
     return np.sum(errors)
 
-def model_tn_max_args(params,trainining_norm,errors): 
+def model_tn_max_args(params,trainining_norm,errors):
     loc_ar = params[0]
     scale_ar = params[1]
     loc_de = params[2]
     scale_de = params[3]
- 
+
     num_training_days = len(trainining_norm)
     time_tn = np.linspace(0,23.5,48)/24
-    
+
     cdf_ar=tn_cdf(time_tn, loc_ar, scale_ar)
     cdf_de=tn_cdf(time_tn, loc_de, scale_de)
 
     res = cdf_ar - cdf_de
     res_n = res
 
-    #error = 0  
+    #error = 0
     for ii in range(0,num_training_days):
         day = trainining_norm[ii]
         #error += np.sum(np.power(res_n - day, 2))
@@ -176,7 +176,7 @@ def model_tn_max_args(params,trainining_norm,errors):
     return np.sum(errors)
 
 
-def model_tn_th_max_args(params,trainining_norm,errors): 
+def model_tn_th_max_args(params,trainining_norm,errors):
     loc_ar = params[0]
     scale_ar = params[1]
     loc_de = params[2]
@@ -193,20 +193,20 @@ def model_tn_th_max_args(params,trainining_norm,errors):
     cdf_ar = cdf_ar/thresh
 
     res = cdf_ar - cdf_de
-    res_n = res 
-  
+    res_n = res
+
     for ii in range(0,num_training_days):
         day = trainining_norm[ii]
         errors[ii,:] = np.power(res_n - day, 2)
     return  np.sum(errors)
 
 
-def model_tn_th_ind_max(params,trainining_norm,training_isfull,errors): 
+def model_tn_th_ind_max(params,trainining_norm,training_isfull,errors):
     loc_ar = params[0]
     scale_ar = params[1]
     loc_de = params[2]
     scale_de = params[3]
- 
+
     num_training_days = len(trainining_norm)
     time_tn = np.linspace(0,23.5,48)/24
 
@@ -216,32 +216,32 @@ def model_tn_th_ind_max(params,trainining_norm,training_isfull,errors):
 
     res = cdf_ar - cdf_de
     res_n = res
-    
+
 
     for ii in range(0,num_training_days):
         day = trainining_norm[ii]
         dayisFull=training_isfull[ii]
-        
+
         if dayisFull:
             thresh=params[4+ii]
             cdf_ar_th=cdf_ar
             cdf_ar_th[cdf_ar>thresh] = thresh
             cdf_ar_th = cdf_ar_th/thresh
-    
+
             res_th = cdf_ar_th - cdf_de
             res_th_n = res_th#/sum(res_th)
-            
+
             errors[ii,:] = np.power(res_th_n - day, 2)
         else:
-            errors[ii,:] = np.power(res_n - day,2)      
+            errors[ii,:] = np.power(res_n - day,2)
     return np.sum(errors)
 
 
 
-def subplot_training(fig, ax, xx, yy, proto_data, test_days, day, proto_name,axis_ylim): 
+def subplot_training(fig, ax, xx, yy, proto_data, test_days, day, proto_name,axis_ylim):
     time = np.linspace(0,23.5,48)
     ax[xx,yy].plot(time, proto_data, linewidth=3, linestyle='dashed', label= proto_name + ' TN prediction')
-    for i in range(0, len(test_days)): 
+    for i in range(0, len(test_days)):
         ax[xx, yy].plot(time, test_days[i], linewidth=1, label='Testing ' + day )
         if i==0:
             ax[xx,yy].legend(fontsize=16)
@@ -249,37 +249,37 @@ def subplot_training(fig, ax, xx, yy, proto_data, test_days, day, proto_name,axi
     ax[xx,yy].set_ylim(-2,axis_ylim)
     ax[xx,yy].set_xlabel('Time (hours)', fontsize=16)
     ax[xx,yy].set_ylabel('Occupancy', fontsize=16)
-    
 
 
-    
+
+
 def plot_model_tn(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1):
     # arrivals
     a_ar = -loc_ar/scale_ar
     b_ar = (1-loc_ar)/scale_ar
-    
+
     # departures
     a_de = -loc_de/scale_de
     b_de = (1-loc_de)/scale_de
-    
+
     time = np.linspace(0,23.5,48)
     time_tn=time/24
-    
+
     pdf_ar = truncnorm.pdf(time_tn, a_ar, b_ar, loc=loc_ar, scale=scale_ar)
     pdf_de = truncnorm.pdf(time_tn, a_de, b_de, loc=loc_de, scale=scale_de)
     cdf_ar = truncnorm.cdf(time_tn, a_ar, b_ar, loc=loc_ar, scale=scale_ar)
     cdf_de = truncnorm.cdf(time_tn, a_de, b_de, loc=loc_de, scale=scale_de)
-    
+
     fig, ax = plt.subplots(2)
     ax[0].plot(time, pdf_ar , '-b')
     ax[0].plot(time, pdf_de, '-r')
     ax[0].set_title('pdfs')
-    
+
     ax[1].plot(time, cdf_ar , '--b')
     ax[1].plot(time, cdf_de, '--r')
     ax[1].plot(time, cdf_ar-cdf_de, 'r')
-    ax[1].set_title('cdfs')   
-       
+    ax[1].set_title('cdfs')
+
 def plot_model_tn_th(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1,thresh=.8):
     # arrivals
     a_ar = -loc_ar/scale_ar
@@ -295,12 +295,12 @@ def plot_model_tn_th(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1,thresh=.8):
     pdf_ar = truncnorm.pdf(time_tn, a_ar, b_ar, loc=loc_ar, scale=scale_ar)
     pdf_de = truncnorm.pdf(time_tn, a_de, b_de, loc=loc_de, scale=scale_de)
     cdf_ar = truncnorm.cdf(time_tn, a_ar, b_ar, loc=loc_ar, scale=scale_ar)
-    
+
     ix_parking_full= np.argmax(cdf_ar>thresh)
     pdf_ar[cdf_ar>thresh] =0
     cdf_ar[cdf_ar>thresh] = thresh
     cdf_ar = cdf_ar/thresh
-    
+
     cdf_de = truncnorm.cdf(time_tn, a_de, b_de, loc=loc_de, scale=scale_de)
 
     fig, ax = plt.subplots(2)
@@ -314,12 +314,12 @@ def plot_model_tn_th(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1,thresh=.8):
     ax[1].plot(time, cdf_ar-cdf_de, 'r')
     ax[1].plot(0.5*ix_parking_full*np.array([1, 1]),[0,1],'--')
     ax[1].set_title('cdfs')
-    
-    
+
+
 def plot_model_tn_thDisc(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1,thresh=.8):
     # arrivals
     fsize=16
-    
+
     a_ar = -loc_ar/scale_ar
     b_ar = (1-loc_ar)/scale_ar
 
@@ -333,33 +333,33 @@ def plot_model_tn_thDisc(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1,thresh=
     pdf_ar = truncnorm.pdf(time_tn, a_ar, b_ar, loc=loc_ar, scale=scale_ar)
     pdf_de = truncnorm.pdf(time_tn, a_de, b_de, loc=loc_de, scale=scale_de)
     cdf_ar = truncnorm.cdf(time_tn, a_ar, b_ar, loc=loc_ar, scale=scale_ar)
-    
-    
+
+
     ix_parking_full= np.argmax(cdf_ar>thresh)
     t_parking_full=np.interp(thresh,cdf_ar,time)
     time_th=np.insert(time,ix_parking_full,t_parking_full)
-  
+
     pdf_ar_orig=pdf_ar.copy()
     pdf_ar=np.insert(pdf_ar,ix_parking_full,np.interp(t_parking_full,time,pdf_ar))
     pdf_ar_excees=pdf_ar.copy()
     pdf_ar[ix_parking_full+1:] =0
     pdf_ar_excees[:ix_parking_full] =0
-    
+
     masktn_arr = np.ones(len(pdf_ar), dtype=bool)
     masktn_arr[ix_parking_full+1:] =False
     masktn_arr_excees = np.ones(len(pdf_ar), dtype=bool)
     masktn_arr_excees[:ix_parking_full] =False
-    
- 
+
+
     cdf_ar=np.insert(cdf_ar,ix_parking_full,thresh)
     cdf_ar_withExcess=cdf_ar.copy()
     cdf_ar[cdf_ar>thresh] = thresh
     cdf_ar = cdf_ar/thresh
     cdf_ar_withExcess=cdf_ar_withExcess/thresh
-    
+
     cdf_de = truncnorm.cdf(time_tn, a_de, b_de, loc=loc_de, scale=scale_de)
-    cdf_de=np.insert(cdf_de,ix_parking_full,np.interp(t_parking_full,time,cdf_de))  
-    
+    cdf_de=np.insert(cdf_de,ix_parking_full,np.interp(t_parking_full,time,cdf_de))
+
     ymin=-0.01
     ymax=max(pdf_ar_orig/sum(pdf_ar_orig))*1.05
 
@@ -375,7 +375,7 @@ def plot_model_tn_thDisc(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1,thresh=
     ax[0].grid(which='major',linestyle='dotted')
     ax[0].set_xlim([0,23.5])
     ax[0].set_ylim([ymin,ymax])
-    
+
     ymin=-0.06
     ymax=max(cdf_ar_withExcess)*1.05
     ax[1].plot(time_th, cdf_ar , '--b',label='cdf TN arrival')
@@ -390,7 +390,7 @@ def plot_model_tn_thDisc(loc_ar=.3, scale_ar=.05, loc_de=.8, scale_de=.1,thresh=
     ax[1].set_xlim([0,23.5])
     ax[1].set_ylim([ymin,ymax])
     ax[1].set_xlabel('hour', fontsize = fsize)
-    ax[1].set_ylabel('porp. of cars', fontsize = fsize)  
+    ax[1].set_ylabel('porp. of cars', fontsize = fsize)
     return fig
 def printTimes(params,current_parking,timeString='WEEKDAYS'):
     print("--------- "+timeString +" "+current_parking+" -----------")
@@ -398,7 +398,7 @@ def printTimes(params,current_parking,timeString='WEEKDAYS'):
     scale_ar = params[1]*24
     loc_de = params[2]*24
     scale_de = params[3]*24
-    
+
     time = np.linspace(0,23.5,48)
     time_tn=time/24
 
@@ -411,19 +411,34 @@ def printTimes(params,current_parking,timeString='WEEKDAYS'):
         if thresh<1:
             cdf_ar = tn_cdf(time_tn, loc_ar/24, scale_ar/24)
             time_parking_full= 0.5*np.argmax(cdf_ar>thresh)
-            print(f'Parking full        = {int(time_parking_full):02.0f}:{int((time_parking_full-int(time_parking_full))*60):02.0f}h')    
-            
-            
+            print(f'Parking full        = {int(time_parking_full):02.0f}:{int((time_parking_full-int(time_parking_full))*60):02.0f}h')
+
+
 def compute_testing_prop_errorM(testing_days, proto_data, m_value):
     errors = np.zeros(48)
     n_test_days = len(testing_days)
     proto = np.array(proto_data)
-    
+
     for i in range(0, n_test_days):
         day = np.array(testing_days[i])
         er = np.array((np.absolute(proto - day)/m_value)*100)
         errors += er
     return errors/n_test_days
+
+def compute_testing_prop_errorMstdv(testing_days, proto_data, m_value):
+    errors = np.zeros(48)
+    errors2=np.zeros(48)
+    n_test_days = len(testing_days)
+    proto = np.array(proto_data)
+
+    for i in range(0, n_test_days):
+        day = np.array(testing_days[i])
+        er = np.array((np.absolute(proto - day)/m_value)*100)
+        errors += er
+        errors2 += er*er
+    meanE=errors/n_test_days
+    stdvE=np.sqrt(errors2/n_test_days-meanE*meanE)
+    return [meanE, stdvE]
 
 def subplotCDFsubtractionErr(fig, ax, axx, axy, x, error, mean, title, day ):
     ax[axx,axy].plot(x, error, color="tomato", linewidth=2, zorder=10, label='Proportional error')
@@ -435,8 +450,27 @@ def subplotCDFsubtractionErr(fig, ax, axx, axy, x, error, mean, title, day ):
     ax[axx,axy].set_ylim((0,1.1*max(error)))
     ax[axx,axy].legend(fontsize=16)
     ax[axx,axy].tick_params( labelsize=15)
-    
 
+def subplotCDFsubtractionStdvErr(fig, ax, axx, axy, x, error, std_error, mean, title, day,
+                                 bolxlim=True,bol_ylabel=True,bol_xlabel=True, bol_legend=True):
+    ax[axx,axy].plot(x, error, color="r", linewidth=2, zorder=10, label='Proportional error')
+    ax[axx,axy].plot(x, error+std_error, color="b", linestyle='--', linewidth=1, zorder=10, label='± stdv')
+    ax[axx,axy].plot(x, error-std_error, color="b", linestyle='--', linewidth=1, zorder=10)
+    ax[axx,axy].plot(x, mean, linewidth=2, linestyle='-.' ,color='black', label='Mean error')
+    ax[axx,axy].grid(linestyle='dotted')
+    if bol_ylabel:
+        ax[axx,axy].set_ylabel('Proportional error %', fontsize=20)
+    if bol_xlabel:
+        ax[axx,axy].set_xlabel('Time [h]', fontsize=20)
+    if not bolxlim:
+        ax[axx,axy].xaxis.set_ticklabels([])
+    ax[axx,axy].set_title(title , fontsize=20, pad=10)
+    ax[axx,axy].set_ylim((0,1.1*max(error+std_error)))
+    ax[axx,axy].set_xlim((0,24))
+    if bol_legend:
+         ax[axx,axy].legend(bbox_to_anchor=(0.7,0.15), loc="lower right",
+                bbox_transform=fig.transFigure, ncol=1,fontsize=16)
+    ax[axx,axy].tick_params( labelsize=15)
 
 def real_timing_predition(fig, ax, axx, day, tn_proto, real_day, scaled_proto, Prototype, limit_hour, t_date, current_parking="LABEL PARKING"):
     time = np.linspace(0,23.5,48)
